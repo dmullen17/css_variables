2f7d199d9a144636ea00680e660fbf4d050f5822
dmullen17 <dmullen17@gmail.com>
2018-12-21 14:45:19 -0800

    finished project

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ blur.addEventListener('change', function(event) {
     console.log(this.value); //number between 0:100
     document.documentElement.style.setProperty('--blur', `${this.value}px`);
 })
+
+let spacing = document.querySelector(`[name="padding"]`);
+spacing.addEventListener('change', function(event) {
+    document.documentElement.style.setProperty('--padding', `${this.value}px`);
+})
+
+let color = document.querySelector(`[name="bkg-color"]`); 
+color.addEventListener('change', function(event) {
+    document.documentElement.style.setProperty('--background-color', this.value);
+})
