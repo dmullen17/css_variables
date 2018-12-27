222ab36b905536c866156cb0267fb958f9f350a5
dmullen17 <dmullen17@gmail.com>
2018-12-27 14:44:28 -0800

    updated solution

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log('hi');
+/*
 let blur = document.querySelector(`[name="blur"]`); 
 // console.log(blur);
 blur.addEventListener('change', function(event) {
@@ -12,7 +12,42 @@ spacing.addEventListener('change', function(event) {
     document.documentElement.style.setProperty('--padding', `${this.value}px`);
 })
 
-let color = document.querySelector(`[name="bkg-color"]`); 
+let color = document.querySelector(`[name="background-color"]`); 
 color.addEventListener('change', function(event) {
     document.documentElement.style.setProperty('--background-color', this.value);
 })
+*/
+
+
+//---------------------------------------------------------------------------------
+// My second solution: 
+// Wrap the <input> elements in a div with class "controls"
+/*
+const inputs = document.querySelectorAll('.controls input'); 
+console.log(inputs);
+inputs.forEach(el => el.addEventListener('change', function(event) {
+    console.log(event);
+    console.log(this);
+    // We need to set the 'name' attribute on inputs equal to the CSS variable names for this logic to work
+    console.log(this.name);
+    // Add px to the value if necessary 
+    const value = this.name === 'background-color' ? this.value : `${this.value}px`;
+    document.documentElement.style.setProperty(`--${this.name}`, value);
+}))
+console.log(inputs);
+*/
+//---------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------
+// Wes Bos Solution
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, `${this.value}${suffix}`);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpate));
+
+//---------------------------------------------------------------------------------
